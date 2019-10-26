5e96e6ddf83356866e543b86072ab8cdca388d78
kenzonawa <vckznw@gmail.com>
2019-10-26 15:51:58 -0700

    background

---
+++ src/components/Landing/landing.js
@@ -0,0 +1,21 @@
+import React from "react"
+import PropTypes from "prop-types"
+import styles from "./landing.module.css"
+
+const Landing = ({ children }) => {
+  return (
+    <>
+      <div className={styles.screen}>
+        <div className="container">
+            <main>{children}</main>
+        </div>
+      </div>
+    </>
+  )
+}
+
+Landing.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Landing
