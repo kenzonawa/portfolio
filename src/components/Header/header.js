9a70ed179c0a895e58b0b6fdc312d1c6cb3afeec
kenzonawa <vckznw@gmail.com>
2019-11-03 19:06:44 -0800

    pre catastrophe

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,6 +1,7 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Pdf from "../../Kenzo_Nawa_Product_Designer_Resume.pdf"
 
 import styles from "./header.module.css"
 
@@ -11,7 +12,9 @@ const Header = ({ siteTitle }) => (
       <div className={styles.links}>
         <Link to="/home/">Work</Link>
         <a href="#about">About</a>
-        <a href="#contact">Contact</a>
+        <a href={Pdf} target="_blank">
+          Resume
+        </a>
       </div>
     </div>
   </header>
