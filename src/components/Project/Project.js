92c792a299ecc8c3dd87e8b4b91ba885ba151320
kenzonawa <vckznw@gmail.com>
2019-11-06 23:14:43 -0800

    redesign 1.0

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -5,23 +5,27 @@ import "../../styles/skeleton.css"
 import Button from "../Button/Button"
 import { Link } from "gatsby"
 
+var classnames = require("classnames")
+
 const Project = props => {
+  let classes = classnames(styles.project, "row")
+
   return (
     <>
-      <div className={styles.divider} />
-      <div className="row">
+      <div className={classes}>
+        <div className="six columns">
+          <h1 className={styles.title}>{props.title}</h1>
+          <h2 className={styles.subtitle}>{props.subtitle}</h2>
+          <p className={styles.description}>{props.description}</p>
+          <Link to={props.link}>
+            <Button primary>Case Study</Button>
+          </Link>
+        </div>
         <div className="six columns">
           <Link to={props.link}>
             <div className={styles.card}>
               <img className={styles.image} src={props.image} alt="project" />
             </div>
-          </Link>
-        </div>
-        <div className="six columns">
-          <h1>{props.title}</h1>
-          <p>{props.description}</p>
-          <Link to={props.link}>
-            <Button primary>Case Study</Button>
           </Link>
         </div>
       </div>
--- src/pages/home.js
+++ src/pages/home.js
@@ -4,25 +4,63 @@ import { Link } from "gatsby"
 import Layout from "../components/Layout/layout"
 import SEO from "../components/seo"
 import Project from "../components/Project/Project"
+import styles from "./styles/home.module.css"
+import Button from "../components/Button/Button"
+import lottie from "lottie-web"
+import Lottie from "react-lottie"
+import animation from "./animation.json"
 
-const Home = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div className="container">
-      <Project
-        image="https://i.imgur.com/iqnYzLw.png"
-        title="This is a project"
-        description="This is the description of this project."
-        link="/intuit/"
-      />
-      <Project
-        image="https://i.imgur.com/iqnYzLw.png"
-        title="Remember"
-        description="Building and launching my own product."
-        link="/remember/"
-      />
-    </div>
-  </Layout>
-)
+const Home = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  }
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div className="homeContainer">
+        <div className="row">
+          <div className="seven columns">
+            <h1 className={styles.kenzo}>
+              Kenzo <br />
+              Nawa
+            </h1>
+            <h3 className={styles.designer}>
+              Product Designer at <span className={styles.intuit}>Intuit.</span>
+            </h3>
+            <p>
+              I strive to push the world forward by creating a bridge between
+              people and technology.
+            </p>
+            <Button primary>Get in touch</Button>
+          </div>
+          <div className="five columns">
+            <Lottie options={defaultOptions} height={300} width={300} />
+          </div>
+        </div>
+        <br />
+        <Project
+          image="https://i.imgur.com/iqnYzLw.png"
+          title="Intuit"
+          subtitle="Redefining design systems"
+          description="How do you help multiple teams on different tech stacks achieve a cohesive process for building out their products?"
+          link="/intuit/"
+        />
+        <Project
+          image="https://i.imgur.com/iqnYzLw.png"
+          title="Remember"
+          subtitle="Launching my own iOS app"
+          description="Building and launching my own product."
+          link="/remember/"
+        />
+      </div>
+    </Layout>
+  )
+}
 
 export default Home
